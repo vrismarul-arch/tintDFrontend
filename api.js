3c5ee3930f064343d30f7eb6cca2446dc5af75f8
vrismarul-arch <vrismarul@gmail.com>
2025-09-09 11:43:43 +0530

    app icom

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const backendURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 const api = axios.create({
   baseURL: backendURL,
 });
-
+/* base */
 // Add a request interceptor to attach the auth token
 api.interceptors.request.use(
   (config) => {
