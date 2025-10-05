0bd37df415fcd1926978634c9726171b2a8b190d
vrismarul-arch <vrismarul@gmail.com>
2025-10-06 00:54:24 +0530

    Fix react-leaflet dependency issusse

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ import Categories from "./categories/Categories";
 import Hero from "./hero/Hero";
 import Testimonial from "../testimonial/Testimonial";
 import TestimonialSlider from "../testimonial/TestimonialSlider";
-import Tintpartner from "../b2b/tintpartner";
+import Tintpartner from "../b2b/Tintpartner";
 
 export default function Home() {
   return (
